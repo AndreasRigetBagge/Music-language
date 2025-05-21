8c96fa7bf779cf970b2f94eb3bee0189a025ab4e
Marcus Skjold <marcusskjold@gmail.com>
2025-05-21 22:44:18 +0200

    Include src code folder for previous commit (gitignore exception)
    Talking about f93115f938fdd4c7f4cc1727842629b9e0889669.
    We ignore other src/ folders, because it is usually generated code.
    Not this, however!

---
+++ eclipse-development/muslan.query.interpreter/src/muslan/query/interpreter/QueryInterpreterException.java
@@ -0,0 +1,9 @@
+package muslan.query.interpreter;
+
+public class QueryInterpreterException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public QueryInterpreterException(String message) {
+		super(message);
+	}
+}
